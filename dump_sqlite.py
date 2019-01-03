c01cbc4c69171146bfb6c047e4a22e43c0ad8c7f
jusing <giuliauni@gmail.com>
2019-01-03 02:04:42 +0100

    input parameters parsing and functions to write output and dump

--- dump_sqlite.py
+++ dump_sqlite.py
@@ -1,18 +1,30 @@
 import os, sys, time, sqlite3
 from collections import defaultdict as dd
 from lxml import etree
+import argparse
 
 ################################################################################
 # TO DO
 ################################################################################
-# - get the db path as input
-# - get output as a file
 # - get sentiment on concepts
 # - get chunks
 # - get sentiment chunks
 # - rename DTD to user_stamp time_stamp
 # - 
 ################################################################################
+
+def create_output(corpus):
+    output = """<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE Corpus SYSTEM "ntumc.dtd">"""
+    output += etree.tostring(corpus, pretty_print=True).decode()
+
+    return output
+
+
+def dump_tree_to_file(output, filename='dump.xml'):
+    with open(filename, 'w') as so:
+        so.write(output)
+
+    print(f'{filename} successfully created')
 
 def inf_dd():
     return dd(inf_dd)
@@ -27,178 +39,175 @@ def valid_xml(c):
         0x10000 <= codepoint <= 0x10FFFF
         )
 
+def main(db_path):
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(cwd, db_path)
 
-################################################################################
-# CONNECT TO DB
-################################################################################
-db_path =  "eng.db" 
-con = sqlite3.connect(db_path)
-c = con.cursor()
-################################################################################
+    ################################################################################
+    # CONNECT TO DB
+    ################################################################################
+    with sqlite3.connect(db_path) as con:
+        c = con.cursor()
+    ################################################################################
 
-c.execute("""SELECT docid,doc,title,url, subtitle, corpusID 
-             FROM doc
-          """)
-sql_docs = c.fetchall()
+        c.execute("""SELECT docid,doc,title,url, subtitle, corpusID 
+                     FROM doc
+                  """)
+        sql_docs = c.fetchall()
 
-c.execute("""SELECT sid, docID, pid, sent, comment, usrname 
-             FROM sent
-          """)
-sql_sents = c.fetchall()
-sents_by_doc_dd = dd(list)
-for s in sql_sents:
-    sents_by_doc_dd[s[1]].append(s)
+        c.execute("""SELECT sid, docID, pid, sent, comment, usrname 
+                     FROM sent
+                  """)
+        sql_sents = c.fetchall()
+        sents_by_doc_dd = dd(list)
+        for s in sql_sents:
+            sents_by_doc_dd[s[1]].append(s)
 
-c.execute("""SELECT sid, wid, word, pos, lemma, cfrom, cto, comment, usrname 
-             FROM word
-          """)
-sql_words = c.fetchall()
-words_by_sent_dd = dd(list)
-for w in sql_words:
-    words_by_sent_dd[w[0]].append(w)
+        c.execute("""SELECT sid, wid, word, pos, lemma, cfrom, cto, comment, usrname 
+                     FROM word
+                  """)
+        sql_words = c.fetchall()
+        words_by_sent_dd = dd(list)
+        for w in sql_words:
+            words_by_sent_dd[w[0]].append(w)
 
+        c.execute("""SELECT sid, cid, clemma, tag, tags, comment, ntag, usrname
+                     FROM concept
+                  """)
+        sql_concepts = c.fetchall()
+        concepts_by_sent_dd = dd(list)
+        for cc in sql_concepts:
+            concepts_by_sent_dd[cc[0]].append(cc)
 
-c.execute("""SELECT sid, cid, clemma, tag, tags, comment, ntag, usrname
-             FROM concept
-          """)
-sql_concepts = c.fetchall()
-concepts_by_sent_dd = dd(list)
-for cc in sql_concepts:
-    concepts_by_sent_dd[cc[0]].append(cc)
+        c.execute("""SELECT sid, wid, cid, usrname
+                     FROM cwl
+                  """)
+        sql_cwl = c.fetchall()
+        cwl_by_sent_dd = dd(lambda: dd(list))
+        for cwl in sql_cwl:
+            sid, wid, cid, usrname = cwl
+            cwl_by_sent_dd[sid][cid].append(wid)
 
+        ################################################################################
+        # BUILD THE XML
+        ################################################################################
 
-c.execute("""SELECT sid, wid, cid, usrname
-             FROM cwl
-          """)
-sql_cwl = c.fetchall()
-cwl_by_sent_dd = dd(lambda: dd(list))
-for cwl in sql_cwl:
-    sid, wid, cid, usrname = cwl
-    cwl_by_sent_dd[sid][cid].append(wid)
+        corpus = etree.Element("Corpus")
+        corpus.set("corpusID", "ntumc")
+        corpus.set("title", "NTU Multilingual Corpus")
+        corpus.set("language", "multilingual")
 
-    
-    
-################################################################################
-# BUILD THE XML
-################################################################################
+        for d in sql_docs:
 
-corpus = etree.Element("Corpus")
-corpus.set("corpusID", "ntumc")
-corpus.set("title", "NTU Multilingual Corpus")
-corpus.set("language", "multilingual")
+            docid, docname, doctitle, docurl, docsub, doccoll = d
+            doclang = "eng"
 
+            document = etree.SubElement(corpus, "Document")
+            document.set("docID", "d" + str(docid))
+            document.set("doc", docname)
+            document.set("language", doclang)
+            document.set("title", doctitle)
+            if docsub:
+                document.set("subtitle", docsub)
+            if docurl:
+                document.set("url", docurl)
+            if doccoll:
+                document.set("collection", str(doccoll))
 
-for d in sql_docs:
+            for s in sents_by_doc_dd[docid]:
 
-    docid, docname, doctitle, docurl, docsub, doccoll =  d
-    doclang = "eng"
-    
-    document = etree.SubElement(corpus, "Document")
-    document.set("docID","d"+str(docid))
-    document.set("doc", docname)
-    document.set("language", doclang)
-    document.set("title", doctitle)
-    if docsub:
-        document.set("subtitle", docsub)
-    if docurl:
-        document.set("url", docurl)
-    if doccoll:
-        document.set("collection", str(doccoll))
+                sid, docID, pid, sent, comment, user = s
 
+                sentence = etree.SubElement(document, "Sentence")
+                sentence.set("sid", "s" + str(sid))
+                try:
+                    sentence.set("sent", sent)
+                except:
+                    cleaned_sent = ''.join(c for c in sent if valid_xml(c))
+                    sentence.set("sent", cleaned_sent)
+                if pid:
+                    sentence.set("pid", str(pid))
+                if comment:
+                    sentence.set("comment", comment)
+                if user:
+                    sentence.set("last_changed_by", user)
 
-    for s in sents_by_doc_dd[docid]:
+                for w in words_by_sent_dd[sid]:
 
-        sid, docID, pid, sent, comment, user = s
-        
-        sentence = etree.SubElement(document, "Sentence")
-        sentence.set("sid","s"+str(sid))
-        try:
-            sentence.set("sent", sent)
-        except:
-            cleaned_sent = ''.join(c for c in sent if valid_xml(c))
-            sentence.set("sent", cleaned_sent)
-        if pid:
-            sentence.set("pid", str(pid))
-        if comment:
-            sentence.set("comment", comment)
-        if user:
-            sentence.set("last_changed_by", user)
-        
+                    sid, wid, word_surf, pos, lemma, cfrom, cto, comment, user = w
 
+                    word = etree.SubElement(sentence, "Word")
+                    word.set("wid", "s" + str(sid) + "w" + str(wid))
 
-        for w in words_by_sent_dd[sid]:
-            
-            sid, wid, word_surf, pos, lemma, cfrom, cto, comment, user = w
+                    try:
+                        word.set("surface_form", word_surf)
+                    except:
+                        cleaned_word = ''.join(c for c in word_surf if valid_xml(c))
+                        word.set("surface_form", cleaned_word)
+                    if pos:
+                        word.set("pos", pos)
+                    if lemma:
+                        word.set("lemma", lemma)
+                    if cfrom:
+                        word.set("cfrom", str(cfrom))
+                    if cto:
+                        word.set("cto", str(cto))
+                    if comment:
+                        word.set("comment", comment)
+                    if user:
+                        word.set("last_changed_by", user)
 
-            word = etree.SubElement(sentence, "Word")
-            word.set("wid", "s"+str(sid)+"w"+str(wid))
+                for cc in concepts_by_sent_dd[sid]:
+                    sid, cid, clemma, tag, tags, comment, ntag, user = cc
 
-            try:
-                word.set("surface_form", word_surf)
-            except:
-                cleaned_word = ''.join(c for c in word_surf if valid_xml(c))
-                word.set("surface_form", cleaned_word)
-            if pos:
-                word.set("pos", pos)
-            if lemma:
-                word.set("lemma", lemma)
-            if cfrom:
-                word.set("cfrom", str(cfrom))
-            if cto:
-                word.set("cto", str(cto))
-            if comment:
-                word.set("comment", comment)
-            if user:
-                word.set("last_changed_by", user)
+                    concept = etree.SubElement(sentence, "Concept")
+                    concept.set("cid", "s" + str(sid) + "c" + str(cid))
 
+                    wids = ""
+                    for wid in cwl_by_sent_dd[sid][cid]:
+                        wids += "s" + str(sid) + "w" + str(wid) + " "
+                    wids = wids.strip()
+                    concept.set("wid", wids)
 
-        for cc in concepts_by_sent_dd[sid]:
-            sid, cid, clemma, tag, tags, comment, ntag, user = cc
-            
-            concept = etree.SubElement(sentence, "Concept")
-            concept.set("cid","s"+str(sid)+"c"+str(cid))
+                    if clemma:
+                        concept.set("clemma", clemma)
+                    if tag:
+                        concept.set("synset_tag", tag)
+                    if comment:
+                        concept.set("comment", comment)
+                    if user:
+                        concept.set("last_changed_by", user)
 
-            wids = ""
-            for wid in cwl_by_sent_dd[sid][cid]:
-                wids += "s"+str(sid)+"w"+str(wid)+" "
-            wids = wids.strip()
-            concept.set("wid", wids)
+                        # concept_tag = etree.SubElement(concept, "Tag")
+                # concept_tag.set("category","sentiment")
+                # concept_tag.set("value","0.8")
 
-            if clemma:
-                concept.set("clemma", clemma) 
-            if tag:
-                concept.set("synset_tag", tag)
-            if comment:
-                concept.set("comment", comment)
-            if user:
-                concept.set("last_changed_by", user) 
-                
-                
+                # chunk = etree.SubElement(sentence, "Chunk")
+                # chunk.set("chid","ch1")
+                # chunk.set("wid","w2 w3")
 
-
-
-            
-        # concept_tag = etree.SubElement(concept, "Tag")
-        # concept_tag.set("category","sentiment")
-        # concept_tag.set("value","0.8")
+                # chunk_tag = etree.SubElement(chunk, "Tag")
+                # chunk_tag.set("category","sentiment")
+                # chunk_tag.set("value","0.8")
 
 
-        # chunk = etree.SubElement(sentence, "Chunk")
-        # chunk.set("chid","ch1")
-        # chunk.set("wid","w2 w3")
+    return corpus
 
+if __name__ == '__main__':
+    usage = "Correct usage: python dump_sqlite.py.py -d <path_to_db_file>"
+    parser = argparse.ArgumentParser(description="Dumps given sqlite file to xml validated against ntumc.dtd")
+    parser.add_argument("-d", "--database", help="path to database file")
+    parser.add_argument("-o", "--output", help="output to file in current folder", action="store_true", default=False)
+    options = parser.parse_args()
 
-        # chunk_tag = etree.SubElement(chunk, "Tag")
-        # chunk_tag.set("category","sentiment")
-        # chunk_tag.set("value","0.8")
+    if options.database:
+        db_path = options.database
+        corpus = main(db_path)
+        output = create_output(corpus)
 
-
-
-
-
-
-
-
-print("""<?xml version="1.0" encoding="UTF-8"?>""")
-print("""<!DOCTYPE Corpus SYSTEM "ntumc.dtd">""")
-print(etree.tostring(corpus, pretty_print=True).decode())
+        if options.output:
+            filename = os.path.basename(db_path).split(".sqlite")[0] + '.xml'
+            dump_tree_to_file(output, filename)
+    else:
+        print(usage)
+        sys.exit(0)
